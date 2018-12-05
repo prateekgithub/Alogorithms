bfe62fa9ba27b6851abd3016183ac2f19e68faf4
Prateek Pande <>
2018-12-05 06:12:28 +0530

    Add files

--- HFP/hello_flask.py
+++ HFP/hello_flask.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello() -> str:
     return 'Hello world from flask'
+
+@app.route('/search4')
+def do_search() -> set:
+    return search4letters(“life, the universe, and everything”, 'eiru,!')
 
 app.run()
