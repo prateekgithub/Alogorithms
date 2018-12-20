f26be01ed45df6ed00e90c4c6d2deaffbf8060df
Prateek Pande <>
2018-12-20 07:56:56 +0530

    Commit All

--- HFP/webapp/vsearch4web.py
+++ HFP/webapp/vsearch4web.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 
 app = Flask(__name__)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log_stream:
+        print('Request is: %s Results are: %s'%(req,res), file = log_stream)
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -11,6 +15,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase = phrase,
                            the_letters = letters,
@@ -21,11 +26,15 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+    return render_template('entry.html', 
+                           the_title='Welcome to search4letters on the web!')
 
-def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log_stream:
-        print('Phrase is: %s Letters are: %s Result is: %s'%(flask_request.form['phrase'], flask_request.form['letters'], str(search4letters(phrase, letters))), file = log_stream)
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
 
 if __name__=='__main__':
     app.run(debug=True)
